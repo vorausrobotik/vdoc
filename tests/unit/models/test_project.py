0841e437d815f524789cbc6d5642a66525888d2c
Jan-Frederik Schmidt <jan-frederik.schmidt@vorausrobotik.com>
2024-12-13 15:02:11 +0100

    feat: Add projects model and exceptions

---
+++ tests/unit/models/test_project.py
@@ -0,0 +1,26 @@
+"""Contains all tests for the project models."""
+
+from pathlib import Path
+
+from packaging.version import Version
+
+from vdoc.models.project import Project
+from tests.conftest import DUMMY_DOCS_STRUCTURE
+
+
+def test_list_projects(dummy_projects_dir: Path) -> None:  # pylint: disable=unused-argument
+    projects = Project.list()
+    assert projects == [Project(name=project_name) for project_name in DUMMY_DOCS_STRUCTURE]
+
+
+def test_list_project_versions(dummy_projects_dir: Path) -> None:  # pylint: disable=unused-argument
+    assert Project(name="dummy-project-01").versions == [
+        Version("0.0.1"),
+        Version("0.0.2"),
+        Version("0.1.0"),
+        Version("0.2.0"),
+    ]
+
+
+def test_get_project_latest_version(dummy_projects_dir: Path) -> None:  # pylint: disable=unused-argument
+    assert Project(name="dummy-project-02").latest == Version("6.0.0")
