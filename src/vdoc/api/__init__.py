53b55fc7aa0c1df6b5252d9a51a012a858fa4872
Jan-Frederik Schmidt <jan-frederik.schmidt@vorausrobotik.com>
2024-12-13 15:05:25 +0100

    feat: Add basic REST API for projects

---
+++ src/vdoc/api/__init__.py
@@ -0,0 +1,80 @@
+"""Contains the Rest API."""
+
+import logging
+from pathlib import Path
+from typing import Any
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
+from fastapi.routing import Mount
+from fastapi.staticfiles import StaticFiles
+from starlette.requests import Request
+from starlette.responses import RedirectResponse
+from starlette.routing import BaseRoute
+
+from vdoc.api.routes import projects as DOCUMENTATION_MODULE
+from vdoc.exceptions import VDocException
+from vdoc.settings import VDocSettings
+
+_PACKAGE_PATH = Path(__file__).parent.parent
+
+webapp_path = _PACKAGE_PATH / "webapp"
+docs_path = _PACKAGE_PATH / "docs"
+
+routes: list[BaseRoute] = [
+    Mount(
+        "/app",
+        app=StaticFiles(directory=str(webapp_path), html=True, check_dir=False),
+        name="ui",
+    ),
+    Mount(
+        "/docs",
+        app=StaticFiles(directory=str(docs_path), html=True, check_dir=False),
+        name="vdoc-docs",
+    ),
+    Mount(
+        "/projects/",
+        app=StaticFiles(directory=VDocSettings().docs_dir.as_posix(), html=True, check_dir=False),
+        name="projects",
+    ),
+]
+
+app = FastAPI(routes=routes, docs_url="/apidoc")
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(DOCUMENTATION_MODULE.router, prefix="/api")
+
+
+@app.exception_handler(VDocException)
+async def unicorn_exception_handler(_: Request, exc: VDocException) -> JSONResponse:
+    """Catches all ``VDocException`` exceptions and returns them as properly formatted JSONResponse.
+
+    Args:
+        exc: The catched exception.
+
+    Returns:
+        The exception as formatted JSONResponse.
+    """
+    return JSONResponse(status_code=exc.status_code, content={"message": exc.detail})
+
+
+@app.get("/")
+def redirect_root_to_app(request: Request) -> RedirectResponse:
+    """Redirects everything from root to app route.
+
+    Args:
+        request: The incoming request.
+
+    Returns:
+        RedirectResponse: Redirection to the app route.
+    """
+    return RedirectResponse(f"app/?{request.query_params}")
