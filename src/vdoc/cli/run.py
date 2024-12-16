8878a42a66fd6521431bbaec990428c79b62d6d6
Jan-Frederik Schmidt <jan-frederik.schmidt@vorausrobotik.com>
2024-12-16 12:01:00 +0100

    feat: Add basic CLI using `typer`

---
+++ src/vdoc/cli/run.py
@@ -0,0 +1,29 @@
+"""This module defines the typer run method."""
+
+import logging
+from typing import Annotated
+
+import typer
+
+from vdoc.methods.cli.cli_run_method import run_impl
+
+_logger = logging.getLogger(__name__)
+
+
+def _cli_run(
+    bind_address: Annotated[
+        str,
+        typer.Option(help="Application bind ip address."),
+    ] = "0.0.0.0",
+    bind_port: Annotated[
+        int,
+        typer.Option(help="Application bind port."),
+    ] = 8080,
+) -> None:  # noqa: disable=D103
+    try:
+        run_impl(bind_address=bind_address, bind_port=bind_port)
+    except KeyboardInterrupt as error:
+        raise typer.Exit(0) from error
+    except Exception as error:
+        _logger.error(error)
+        raise typer.Exit(1) from error
