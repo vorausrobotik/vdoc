fbb75ae09f4494af7fdbec8b6221b2cfe300ec7b
Jan-Frederik Schmidt <jan-frederik.schmidt@vorausrobotik.com>
2024-12-18 09:53:28 +0100

    refactor: Move constant variables to `constants.py` file

--- src/vdoc/settings/__init__.py
+++ src/vdoc/settings/__init__.py
@@ -4,10 +4,12 @@ from pathlib import Path
 
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+from vdoc.constants import CONFIG_ENV_PREFIX, DEFAULT_DOCS_DIR
+
 
 class VDocSettings(BaseSettings):
     """The vdoc settings."""
 
-    model_config = SettingsConfigDict(env_prefix="VDOC_")
+    model_config = SettingsConfigDict(env_prefix=CONFIG_ENV_PREFIX)
 
-    docs_dir: Path = Path("/srv/vdoc/docs/")
+    docs_dir: Path = DEFAULT_DOCS_DIR
