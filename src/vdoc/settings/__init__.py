f863423782910b5ccead6da1de6b8ee5c46faa09
Jan-Frederik Schmidt <jan-frederik.schmidt@vorausrobotik.com>
2025-01-09 15:09:17 +0100

    feat: Allow configuring bind address and bind port

--- src/vdoc/settings/__init__.py
+++ src/vdoc/settings/__init__.py
@@ -4,7 +4,14 @@ from pathlib import Path
 
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-from vdoc.constants import CONFIG_ENV_PREFIX, DEFAULT_API_PASSWORD, DEFAULT_API_USERNAME, DEFAULT_DOCS_DIR
+from vdoc.constants import (
+    CONFIG_ENV_PREFIX,
+    DEFAULT_API_PASSWORD,
+    DEFAULT_API_USERNAME,
+    DEFAULT_BIND_ADDRESS,
+    DEFAULT_BIND_PORT,
+    DEFAULT_DOCS_DIR,
+)
 
 
 class VDocSettings(BaseSettings):
@@ -15,3 +22,5 @@ class VDocSettings(BaseSettings):
     docs_dir: Path = DEFAULT_DOCS_DIR
     api_username: bytes = DEFAULT_API_USERNAME
     api_password: bytes = DEFAULT_API_PASSWORD
+    bind_address: str = DEFAULT_BIND_ADDRESS
+    bind_port: int = DEFAULT_BIND_PORT
