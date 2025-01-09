726d01f8fe53dd1c96bd46b0b44b29db7f0babc0
Jan-Frederik Schmidt <jan-frederik.schmidt@vorausrobotik.com>
2025-01-09 16:33:30 +0100

    feat(ui): Add dropdown to select project versions to app bar

--- src/ui/MenuBar.tsx
+++ src/ui/MenuBar.tsx
@@ -1,31 +1,58 @@
 import AppBar from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
-import { Container } from '@mui/material'
+import { Box } from '@mui/material'
 import VDocLogo from './icons/VDocLogo'
+import { useStore } from '@tanstack/react-store'
+import globalStore from './helpers/GlobalStore'
+import MenuItem from '@mui/material/MenuItem'
+import Select, { SelectChangeEvent } from '@mui/material/Select'
+import { useState } from 'react'
+import FormControl from '@mui/material/FormControl'
+import InputLabel from '@mui/material/InputLabel'
+import IconButton from '@mui/material/IconButton'
+import { useNavigate } from '@tanstack/react-router'
 
 export default function MenuBar() {
+  const projectVersions = useStore(globalStore, (state) => state['projectVersions'])
+  const currentVersion = useStore(globalStore, (state) => state['currentVersion'])
+  const [versionDropdownValue, setVersionDropdownValue] = useState(currentVersion)
+  const navigate = useNavigate({ from: '/$projectName/versions/$version' })
+
+  const handleVersionSelectChange = (event: SelectChangeEvent) => {
+    const selectedVersion = event.target.value
+    setVersionDropdownValue(selectedVersion)
+    navigate({
+      to: `/$projectName/versions/${selectedVersion}`,
+    })
+  }
+
   return (
     <AppBar position="static">
-      <Container maxWidth="xl">
-        <Toolbar disableGutters variant="dense" sx={{ py: 1 }}>
-          <VDocLogo height={50} width={50} viewBox="0 0 100 100" />
-          <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            href={'/'}
-            sx={{
-              ml: 4,
-              textDecoration: 'none',
-              color: 'inherit',
-              display: { xs: 'none', md: 'flex' },
-            }}
-          >
-            vdoc
+      <Toolbar>
+        {/* Logo with Text */}
+        <Box sx={{ display: { xs: 'none', sm: 'flex' }, alignItems: 'center', flexGrow: 0, mr: 2 }}>
+          <IconButton aria-label="delete" href={'/'}>
+            <VDocLogo height={50} width={50} viewBox="0 0 100 100" />
+          </IconButton>
+
+          <Typography variant="h6" component="div">
+            VDoc
           </Typography>
-        </Toolbar>
-      </Container>
+        </Box>
+        <Box sx={{ flexGrow: 1, display: { xs: 'none', sm: 'flex' } }} />
+
+        {/* Dropdown Menu */}
+        <Box sx={{ flexGrow: 0, display: { xs: projectVersions ? 'flex' : 'none' } }}>
+          <FormControl size="small" variant="outlined" sx={{ minWidth: 120 }}>
+            <InputLabel>Version</InputLabel>
+            <Select value={versionDropdownValue} onChange={handleVersionSelectChange} label="Version">
+              <MenuItem value="latest">latest</MenuItem>
+              {projectVersions?.map((projectVersion) => <MenuItem value={projectVersion}>{projectVersion}</MenuItem>)}
+            </Select>
+          </FormControl>
+        </Box>
+      </Toolbar>
     </AppBar>
   )
 }
--- src/ui/routes/$projectName/versions/$version.tsx
+++ src/ui/routes/$projectName/versions/$version.tsx
@@ -1,9 +1,11 @@
 import { useQuery } from '@tanstack/react-query'
+import { useEffect } from 'react'
 import { createFileRoute } from '@tanstack/react-router'
 import DocuCanvas from '../../../DocuCanvas'
 import QueryStateHandler from '../../../helpers/QueryStateHandler'
 import { FastAPIAxiosErrorT } from '../../../interfacesAndTypes/Error'
 import { fetchProjectVersionAndLatestVersion, fetchProjectVersions } from '../../../helpers/APIFunctions'
+import globalStore from '../../../helpers/GlobalStore'
 
 export const Route = createFileRoute('/$projectName/versions/$version')({
   component: DocumentationComponent,
@@ -11,6 +13,18 @@ export const Route = createFileRoute('/$projectName/versions/$version')({
 
 function DocumentationComponent() {
   const { projectName, version } = Route.useParams()
+  useEffect(() => {
+    // Fetch all available versions for the project and put them into the global store object
+    fetchProjectVersions(projectName).then((versions) =>
+      globalStore.setState((state) => {
+        return {
+          ...state,
+          projectVersions: versions,
+          currentVersion: version,
+        }
+      })
+    )
+  }, [projectName, version])
   const {
     data: versions,
     error: versionsError,
