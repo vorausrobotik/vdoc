60726858ec148cf64e7aeeffc5353c210e380bdd
Jan-Frederik Schmidt <jan-frederik.schmidt@vorausrobotik.com>
2025-01-09 15:09:17 +0100

    feat: Allow configuring bind address and bind port

--- src/vdoc/cli/run.py
+++ src/vdoc/cli/run.py
@@ -6,6 +6,7 @@ from typing import Annotated
 import typer
 
 from vdoc.methods.cli.cli_run_method import run_impl
+from vdoc.settings import VDocSettings
 
 _logger = logging.getLogger(__name__)
 
@@ -14,11 +15,11 @@ def _cli_run(
     bind_address: Annotated[
         str,
         typer.Option(help="Application bind ip address."),
-    ] = "0.0.0.0",
+    ] = VDocSettings().bind_address,
     bind_port: Annotated[
         int,
         typer.Option(help="Application bind port."),
-    ] = 8080,
+    ] = VDocSettings().bind_port,
 ) -> None:  # noqa: disable=D103
     try:
         run_impl(bind_address=bind_address, bind_port=bind_port)
