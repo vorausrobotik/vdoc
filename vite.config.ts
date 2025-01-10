b32f64502c5f1bbdea4b3873f305c78d57644d64
Jan-Frederik Schmidt <jan-frederik.schmidt@vorausrobotik.com>
2025-01-10 10:27:03 +0100

    feat: Enable UI asset compression using `vite-plugin-compression2`

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import { compression } from 'vite-plugin-compression2'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     TanStackRouterVite({ routesDirectory: 'src/ui/routes/', generatedRouteTree: 'src/ui/routeTree.gen.ts' }),
     react(),
+    compression(),
   ],
   root: 'src/ui',
   base: process.env.USE_VITE_PROXY ? '' : '/app/',
