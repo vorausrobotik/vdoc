61c529458f3f1cf9bd076438b2c53b65967088c2
Jan-Frederik Schmidt <jan-frederik.schmidt@vorausrobotik.com>
2025-01-31 09:40:48 +0100

    test(ui): Provide an actual index.html file for documentation mocks

--- tests/ui/base.ts
+++ tests/ui/base.ts
@@ -1,40 +1,47 @@
 import { test } from '@playwright/test'
 import { Page } from '@playwright/test'
+import fs from 'fs'
+import path from 'path'
 
 const mockAPIRequests = async (page: Page) => {
   const routes = [
-    { pattern: '*/**/static/projects/*/*/', response: 'Hello, this is documentation content!' },
+    {
+      pattern: '*/**/static/projects/*/*/',
+      response: { body: fs.readFileSync(path.resolve('tests/ui/resources/mockedIndex.html')) },
+    },
     {
       pattern: '*/**/api/projects/',
-      response: [{ name: 'example-project-01' }, { name: 'example-project-02' }, { name: 'example-project-03' }],
+      response: {
+        json: [{ name: 'example-project-01' }, { name: 'example-project-02' }, { name: 'example-project-03' }],
+      },
     },
     {
       pattern: '*/**/api/projects/example-project-01/versions/',
-      response: ['0.1.0', '0.2.0', '1.0.0', '2.0.0', '3.0.0', '3.1.0', '3.2.0'],
+      response: { json: ['0.1.0', '0.2.0', '1.0.0', '2.0.0', '3.0.0', '3.1.0', '3.2.0'] },
     },
     {
       pattern: '*/**/api/projects/example-project-01/versions/latest',
-      response: ['3.2.0'],
+      response: { json: ['3.2.0'] },
     },
     {
       pattern: '*/**/api/projects/example-project-02/versions/',
-      response: ['1.0.0'],
+      response: { json: ['1.0.0'] },
     },
     {
       pattern: '*/**/api/projects/example-project-02/versions/latest',
-      response: ['1.0.0'],
+      response: { json: ['1.0.0'] },
     },
     {
       pattern: '*/**/api/projects/example-project-03/versions/',
-      response: ['0.1.0', '1.0.0'],
+      response: { json: ['0.1.0', '1.0.0'] },
     },
     {
       pattern: '*/**/api/projects/example-project-03/versions/latest',
-      response: ['1.0.0'],
+      response: { json: ['1.0.0'] },
     },
   ]
   for (const { pattern, response } of routes) {
-    await page.route(pattern, (route) => route.fulfill({ json: response }))
+    await page.route(pattern, (route) => route.fulfill(response))
   }
 }
 
