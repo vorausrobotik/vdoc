e1ec52e8657aa331357042943a5e5b302c258deb
Jan-Frederik Schmidt <jan-frederik.schmidt@vorausrobotik.com>
2025-02-03 12:00:39 +0100

    test: Use `sphinxawesome-theme` as example docs theme and misc
    - Fix permalink icon
    - Add version to docu header
    - Remove sphinx footer

--- tests/resources/sample-docs/meta-project/conf.py
+++ tests/resources/sample-docs/meta-project/conf.py
@@ -1,11 +1,14 @@
 import os
 
+from sphinxawesome_theme.postprocess import Icons
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = "Meta Project"
 copyright = "2025, John Doe"
 author = "John Doe"
+release = os.environ.get("INJECTED_VERSION", "unknown")
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -21,7 +24,9 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "alabaster"
+html_theme = "sphinxawesome_theme"
+html_permalinks_icon = Icons.permalinks_icon
+html_show_sphinx = False
 rst_prolog = f"""
 .. |injected_version| replace:: {os.environ.get("INJECTED_VERSION", "unknown")}
 """
