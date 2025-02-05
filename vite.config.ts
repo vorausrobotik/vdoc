14c837af758d5c49d20936bf4349dc9d9065ed42
Jan-Frederik Schmidt <jan-frederik.schmidt@vorausrobotik.com>
2025-02-05 19:01:59 +0100

    ci: Publish npm bundle analysis to codecov.io

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 import { compression } from 'vite-plugin-compression2'
+import { codecovVitePlugin } from '@codecov/vite-plugin'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,6 +10,13 @@ export default defineConfig({
     TanStackRouterVite({ routesDirectory: 'src/ui/routes/', generatedRouteTree: 'src/ui/routeTree.gen.ts' }),
     react(),
     compression(),
+    // Put the Codecov vite plugin after all other plugins
+    codecovVitePlugin({
+      enableBundleAnalysis: process.env.CODECOV_TOKEN !== undefined,
+      bundleName: 'vdoc',
+      uploadToken: process.env.CODECOV_TOKEN,
+      telemetry: false,
+    }),
   ],
   root: 'src/ui',
   build: {
