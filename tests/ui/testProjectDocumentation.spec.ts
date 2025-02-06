77d35a77ceaa03223a4f45b2a1958ec741a0e71d
Jan-Frederik Schmidt <jan-frederik.schmidt@vorausrobotik.com>
2025-02-06 14:31:20 +0100

    feat(ui): Handle non existing project documentations
    Uses the features provided by `@tanstack/react-router`

---
+++ tests/ui/testProjectDocumentation.spec.ts
@@ -0,0 +1,47 @@
+import { expect } from '@playwright/test'
+import test, { prepareTestSuite } from './base'
+import testIDs from '../../src/ui/interfacesAndTypes/testIDs'
+
+await prepareTestSuite(test)
+
+test('Requesting non existing versions must be handled properly with automatic redirect', async ({ page }) => {
+  await page.goto('/')
+  await expect(page).toHaveURL('http://localhost:3000')
+  await page.goto('/example-project-01/42.0.0')
+  await expect(page).toHaveURL('http://localhost:3000/example-project-01/42.0.0')
+
+  await expect(page.getByTestId(testIDs.project.documentation.documentationIframe)).not.toBeVisible()
+
+  await expect(page.getByTestId(testIDs.errorComponent.main)).toBeVisible()
+  expect(await page.getByTestId(testIDs.errorComponent.title).innerText()).toBe(
+    "Project 'example-project-01' doesn't have a documentation for version '42.0.0'"
+  )
+  expect(await page.getByTestId(testIDs.errorComponent.actionButton).innerText()).toBe('GO BACK')
+  for (const remainingSeconds of [5, 4, 3, 2, 1]) {
+    await expect(page.getByTestId(testIDs.errorComponent.description)).toHaveText(
+      `Returning to previous page in ${remainingSeconds} second${remainingSeconds > 1 ? 's' : ''}...`
+    )
+  }
+
+  // User must be redirected to previous page
+  await expect(page).toHaveURL('http://localhost:3000')
+})
+
+test('Requesting non existing versions must be handled properly with manual redirect', async ({ page }) => {
+  await page.goto('/')
+  await expect(page).toHaveURL('http://localhost:3000')
+  await page.goto('/example-project-01/42.0.0')
+  await expect(page).toHaveURL('http://localhost:3000/example-project-01/42.0.0')
+
+  await expect(page.getByTestId(testIDs.project.documentation.documentationIframe)).not.toBeVisible()
+
+  await expect(page.getByTestId(testIDs.errorComponent.main)).toBeVisible()
+  expect(await page.getByTestId(testIDs.errorComponent.title).innerText()).toBe(
+    "Project 'example-project-01' doesn't have a documentation for version '42.0.0'"
+  )
+  expect(await page.getByTestId(testIDs.errorComponent.actionButton).innerText()).toBe('GO BACK')
+  await page.getByTestId(testIDs.errorComponent.actionButton).click()
+
+  // User must be redirected to previous page
+  await expect(page).toHaveURL('http://localhost:3000', { timeout: 2000 })
+})
