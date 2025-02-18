d193e3199e3489846990de077469f8c587d26525
Jan-Frederik Schmidt <jan-frederik.schmidt@vorausrobotik.com>
2025-02-18 10:48:46 +0100

    test(ui): Test that fetching a non-existing or invalid version is handled

--- tests/unit/models/test_project.py
+++ tests/unit/models/test_project.py
@@ -2,9 +2,11 @@
 
 from pathlib import Path
 
+import pytest
 from packaging.version import Version
 
 from tests.conftest import DUMMY_DOCS_STRUCTURE
+from vdoc.exceptions import InvalidVersion, ProjectVersionNotFound
 from vdoc.models.project import Project
 
 
@@ -26,3 +28,27 @@ def test_list_project_versions(dummy_projects_dir: Path) -> None:  # pylint: dis
 
 def test_get_project_latest_version(dummy_projects_dir: Path) -> None:  # pylint: disable=unused-argument
     assert Project(name="dummy-project-02").latest == Version("6.0.0")
+
+
+def test_get_version_and_docs_path(dummy_projects_dir: Path) -> None:  # pylint: disable=unused-argument
+    assert (Version("1.0.0"), dummy_projects_dir / "dummy-project-01" / "1.0.0") == Project.get_version_and_docs_path(
+        name="dummy-project-01", version="1.0.0"
+    )
+
+
+def test_get_version_and_docs_path_latest(dummy_projects_dir: Path) -> None:  # pylint: disable=unused-argument
+    assert (Version("2.0.0"), dummy_projects_dir / "dummy-project-01" / "2.0.0") == Project.get_version_and_docs_path(
+        name="dummy-project-01", version="latest"
+    )
+
+
+def test_get_version_and_docs_path_invalid_version(dummy_projects_dir: Path) -> None:  # pylint: disable=unused-argument
+    with pytest.raises(InvalidVersion):
+        Project.get_version_and_docs_path(name="dummy-project-01", version="abc")
+
+
+def test_get_version_and_docs_path_incomplete_version(
+    dummy_projects_dir: Path,  # pylint: disable=unused-argument
+) -> None:
+    with pytest.raises(ProjectVersionNotFound):
+        Project.get_version_and_docs_path(name="dummy-project-01", version="1")
