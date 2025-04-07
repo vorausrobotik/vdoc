a8b66a434a3936ad8678308d2f1b9bda92ffd6a3
Jan-Frederik Schmidt <jan-frederik.schmidt@vorausrobotik.com>
2025-04-07 11:41:43 +0200

    fix(python-tests): Clear `os.environ` on patching
    Otherwise, potentially locally existing development environment variables
    might override the test behavior.
    
    Note: This cannot be used together with the `dummy_projects_dir` fixture
    since it patches the `os.environ` internally as well.

--- tests/unit/api/routes/test_settings_routes.py
+++ tests/unit/api/routes/test_settings_routes.py
@@ -8,7 +8,7 @@ from fastapi.testclient import TestClient
 from vdoc.constants import DEFAULT_LOGO_DARK_URL, DEFAULT_LOGO_LIGHT_URL
 
 
-@patch.dict(os.environ, {"VDOC_LOGO_LIGHT_URL": "FOO", "VDOC_LOGO_DARK_URL": "BAR"})
+@patch.dict(os.environ, {"VDOC_LOGO_LIGHT_URL": "FOO", "VDOC_LOGO_DARK_URL": "BAR"}, clear=True)
 def test_get_logo_urls(api: TestClient) -> None:
     assert api.get("/api/settings/logo_url/light").json() == "FOO"
     assert api.get("/api/settings/logo_url/dark").json() == "BAR"
