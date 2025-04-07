617931b79c8385475e325a08d95e5668b49a881d
Jan-Frederik Schmidt <jan-frederik.schmidt@vorausrobotik.com>
2025-04-07 11:45:17 +0200

    fix(api): Return the raw string representation in project version list
    The `packaging.version.Version` parser shortens SemVer compatible alpha/...
    tags from `X.Y.Z-beta` to `X.Y.Zb0` and doesn't keep a reference to the
    original raw string. The `list_project_versions_impl` function returned
    the list of parsed versions (keys) of the dictionary and not the raw
    versions (values).

--- tests/unit/methods/api/test_project_methods.py
+++ tests/unit/methods/api/test_project_methods.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 import pytest
 
-from vdoc.methods.api.projects import get_project_version_impl
+from vdoc.methods.api.projects import get_project_version_impl, list_project_versions_impl
 
 
 @pytest.mark.parametrize(
@@ -18,3 +18,7 @@ def test_get_project_version_impl(
     expected_version: str,
 ) -> None:
     assert get_project_version_impl(project_name, requested_version) == expected_version
+
+
+def test_list_project_versions_impl(dummy_projects_dir: Path) -> None:  # pylint: disable=unused-argument
+    assert list_project_versions_impl("dummy-project-03") == ["1.0.0", "1.3.0", "2.0.0-beta"]
