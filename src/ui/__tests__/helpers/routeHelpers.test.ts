1df57824f784ba46b407057d97b69eb067ea6be1
Jan-Frederik Schmidt <jan-frederik.schmidt@vorausrobotik.com>
2025-05-16 10:27:02 +0200

    feat(ui)!: Simplify and re-work `sanitizeDocuUri`
    - The parameter `basePath` is now optional.
    - The parameters `projectName` and `projectVersion` have been renamed to
      `overrideName` and `overrideVersion`. They are optional as well.
    - The complex regex pattern has been replaced by using a `URL` class object

--- src/ui/__tests__/helpers/routeHelpers.test.ts
+++ src/ui/__tests__/helpers/routeHelpers.test.ts
@@ -8,60 +8,67 @@ describe('sanitizeDocuUri', () => {
       {
         input: {
           href: `${basePath}/project-one/6.0/index.html#`,
+        },
+        // Empty hash must be removed
+        expected: {
           projectName: 'project-one',
-          projectVersion: '6.0',
+          version: '6.0',
+          _splat: 'index.html',
+          href: `${basePath}/project-one/6.0/index.html`,
+        },
+      },
+      {
+        input: {
+          href: `project-one/6.0/index.html#id`,
         },
         expected: {
           projectName: 'project-one',
           version: '6.0',
-          _splat: 'index.html#',
-          href: `${basePath}/project-one/6.0/index.html#`,
+          _splat: 'index.html#id',
+          href: 'project-one/6.0/index.html#id',
         },
       },
       {
+        // No reminder (index.html or whatever) must work
         input: {
-          href: `${basePath}/meta-project/1.3.0/#`,
-          projectName: 'meta-project',
-          projectVersion: '1.3.0',
+          href: `${basePath}/meta-project/1.3.0/#id?test=foo`,
         },
         expected: {
           projectName: 'meta-project',
           version: '1.3.0',
-          _splat: '#',
-          href: `${basePath}/meta-project/1.3.0/#`,
+          _splat: '#id?test=foo',
+          href: `${basePath}/meta-project/1.3.0/#id?test=foo`,
         },
       },
       {
         input: {
-          href: `${basePath}/meta-project/1.3.0`,
-          projectName: 'meta-project',
-          projectVersion: '1.3.0',
+          href: `${basePath}/meta-project/1.3.0/index.html#id?foo=bar&bar=foo`,
         },
+        // Hash and search must work
         expected: {
           projectName: 'meta-project',
           version: '1.3.0',
-          _splat: '',
-          href: `${basePath}/meta-project/1.3.0`,
+          _splat: 'index.html#id?foo=bar&bar=foo',
+          href: `${basePath}/meta-project/1.3.0/index.html#id?foo=bar&bar=foo`,
         },
       },
       {
+        // Override name and version must work
         input: {
-          href: `${basePath}/project-one/latest/examples.html#examples`,
-          projectName: 'project-one',
-          projectVersion: 'latest',
+          href: `${basePath}/project-one/1.0.0/examples.html#examples`,
+          overrideVersion: 'latest',
+          overrideName: 'example',
         },
         expected: {
-          projectName: 'project-one',
+          projectName: 'example',
           version: 'latest',
           _splat: 'examples.html#examples',
-          href: `${basePath}/project-one/latest/examples.html#examples`,
+          href: `${basePath}/example/latest/examples.html#examples`,
         },
       },
       {
         input: {
           href: `${basePath}/static/projects/project-one/latest/examples.html#examples`,
-          projectName: 'project-one',
-          projectVersion: 'latest',
         },
         expected: {
           projectName: 'project-one',
@@ -72,14 +79,13 @@ describe('sanitizeDocuUri', () => {
       },
     ]
     testData.forEach(({ input, expected }) => {
-      expect(sanitizeDocuUri(input.href, basePath, input.projectName, input.projectVersion)).toStrictEqual(expected)
+      expect(sanitizeDocuUri(input.href, input.overrideName, input.overrideVersion)).toStrictEqual(expected)
     })
   })
   test('sanitizing invalid or external uris must throw errors', () => {
-    const basePath = 'http://localhost:8080'
     const testData = ['https://google.com', 'http://localhost:9000']
     testData.forEach((href) => {
-      expect(() => sanitizeDocuUri(href, basePath, '', '')).toThrowError(`Unable to sanitize doc URI ${href}`)
+      expect(() => sanitizeDocuUri(href)).toThrowError(`Unable to match URI '${href}'`)
     })
   })
 })
