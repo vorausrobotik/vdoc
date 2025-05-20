a2ccb4b92a3ba9af53c9254953064bbe9393dd04
Jan-Frederik Schmidt <jan-frederik.schmidt@vorausrobotik.com>
2025-05-20 08:52:21 +0200

    test(api): Add tests for serving webapp assets and index.html

--- tests/unit/api/routes/test_misc_routes.py
+++ tests/unit/api/routes/test_misc_routes.py
@@ -4,6 +4,7 @@ from pathlib import Path
 from unittest.mock import MagicMock, patch
 
 from fastapi.testclient import TestClient
+from pytest import MonkeyPatch
 
 
 @patch("vdoc.api.routes.version.get_app_version")
@@ -23,3 +24,19 @@ def test_sphinx_inventory(dummy_projects_dir: Path, api: TestClient) -> None:
 def test_sphinx_inventory_non_existing(api: TestClient) -> None:
     response = api.get("/dummy-project-01/latest/objects.inv")
     assert response.status_code == 404
+
+
+def test_serve_frontend_assets(monkeypatch: MonkeyPatch, tmp_path: Path, api: TestClient) -> None:
+    (tmp_path / "style.css").write_text("dummy style sheet")
+    monkeypatch.setattr("vdoc.api.lifespan.webapp_path", tmp_path)
+    response = api.get("/style.css")
+    assert response.status_code == 200
+    assert response.text == "dummy style sheet"
+
+
+def test_serve_frontend_assets_index_fallback(monkeypatch: MonkeyPatch, tmp_path: Path, api: TestClient) -> None:
+    (tmp_path / "index.html").write_text("dummy index.html content")
+    monkeypatch.setattr("vdoc.api.lifespan.webapp_path", tmp_path)
+    response = api.get("/style.css")
+    assert response.status_code == 200
+    assert response.text == "dummy index.html content"
