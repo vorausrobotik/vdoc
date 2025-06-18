b46691229827c36b069dbd2107d3e0fa0ce9c0e9
Jan-Frederik Schmidt <jan-frederik.schmidt@vorausrobotik.com>
2025-06-18 12:02:01 +0200

    feat(ui): Toggle doxygen awesome color scheme (dark/light)
    Toggling the documentation color mode `dark | light` currently works only
    for sphinx documentations using the [Awesome Sphinx Theme](https://sphinxawesome.xyz/).
    
    With the changes introduced in this PR, the color mode of documentations using [Doxygen Awesome](https://jothepro.github.io/doxygen-awesome-css/)can be toggled as well.

--- src/ui/components/IFrame.tsx
+++ src/ui/components/IFrame.tsx
@@ -27,10 +27,20 @@ export default function IFrame({ src, onPageChanged, onHashChanged, onTitleChang
 
   const setDarkMode = useCallback((mode: 'dark' | 'light') => {
     iframeRef?.current?.contentWindow?.localStorage.setItem('darkMode', mode as 'light' | 'dark')
-    if (mode === 'dark') {
-      iframeRef?.current?.contentDocument?.documentElement?.classList.toggle('dark', true)
-    } else {
-      iframeRef?.current?.contentDocument?.documentElement?.classList.toggle('dark', false)
+    const documentElement = iframeRef?.current?.contentDocument?.documentElement
+    if (!documentElement) {
+      return
+    }
+    // https://jothepro.github.io/doxygen-awesome-css/md_docs_tricks.html#tricks-darkmode
+    const isDoxygen = documentElement?.getAttribute('xmlns') === 'http://www.w3.org/1999/xhtml'
+    if (isDoxygen) {
+      documentElement.classList.remove('light-mode', 'dark-mode')
+      documentElement.classList.add(mode === 'dark' ? 'dark-mode' : 'light-mode')
+    }
+    // If not Doxygen, use the standard dark class (in our case sphinx awesome using tailwind)
+    // https://tailwindcss.com/docs/dark-mode#toggling-dark-mode-manually
+    else {
+      documentElement.classList.toggle('dark', mode === 'dark')
     }
   }, [])
 
